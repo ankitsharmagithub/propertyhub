4d604861da9cb41f409c69381196b6a8ab5dab78
ankitsharmagithub <ankitsharmak87@gmail.com>
2026-08-26 17:17:00 +0530

    remove api/index.php

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$app = require __DIR__ . '/../bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
