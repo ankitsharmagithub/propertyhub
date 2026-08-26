6a1cdbc933f82aeab843a1bd78c6a5a081eda258
ankitsharmagithub <ankitsharmak87@gmail.com>
2026-08-26 17:08:25 +0530

    Add Vercel config

--- api/index.php
+++ api/index.php
@@ -1,15 +1,9 @@
 <?php
 
-// Path to your Laravel application
 $app = require __DIR__ . '/../bootstrap/app.php';
-
-// Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
 );
-
 $response->send();
-
 $kernel->terminate($request, $response);
